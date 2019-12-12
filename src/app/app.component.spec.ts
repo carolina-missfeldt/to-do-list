69919bf5272c1a9e359e1b9346f4af784b6903f5
carolina-missfeldt <carolina.missfeldt@gmail.com>
2019-12-12 15:47:59 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-to-to-list'`, () => {
+  it(`should have as title 'my-to-do-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-to-to-list');
+    expect(app.title).toEqual('my-to-do-list');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-to-to-list!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-to-do-list!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-to-to-list';
+  title = 'my-to-do-list';
 }
